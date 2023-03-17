ec937d13288c9fcf99312aea1db4d43968262c4e
ruhinalio <116165822+ruhinalio@users.noreply.github.com>
2023-03-17 12:06:02 +0400

    some evnts add

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,13 +1,3 @@
-// menu acilmasi ucun
-const projectsMenu = document.querySelector(".projects-menu");
-const projectsSide = document.querySelector(".projects-side");
-
-projectsMenu.addEventListener("click", () => {
-  projectsSide.classList.toggle("active-menu");
-});
-
-
-
 // brand hissesinde renglerin deyismesi ucun
 
 const elements = [...document.querySelectorAll(".brand")];
@@ -18,35 +8,48 @@ elements.forEach((element, index) => {
     elements.forEach((el, i) => {
       if (i === index) {
         el.classList.add("brand-active");
-        
       } else {
         el.classList.remove("brand-active");
       }
     });
-    serviceSide.forEach((el,i) =>{
-        if (i === index) {
-            el.classList.add("branding-active");
-          } else {
-            el.classList.remove("branding-active");
-          }
-    })
+    serviceSide.forEach((el, i) => {
+      if (i === index) {
+        el.classList.add("branding-active");
+      } else {
+        el.classList.remove("branding-active");
+      }
+    });
   });
 });
 
 // slide hissesi ucun
-const slides = document.querySelectorAll('.slide')
+const slides = document.querySelectorAll(".slide");
 
 for (const slide of slides) {
-    slide.addEventListener('click', () => {
-      clearActiveClasses()
-      
-      
-      slide.classList.add('active')
-    })
+  slide.addEventListener("click", () => {
+    clearActiveClasses();
+
+    slide.classList.add("active");
+  });
 }
 
 function clearActiveClasses() {
-    slides. forEach((slide) => {
-        slide. classList. remove('active')
-    })
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
 }
+
+
+// menu hissesinin acilmasi ucun
+const pagesBtn = document.getElementById("pages")
+const projectsBtn = document.getElementById("projects")
+const projectsHtml = document.querySelector(".for-project")
+const pagesHtml = document.querySelector(".for-pages")
+
+
+pagesBtn.addEventListener("click",()=>{
+  pagesHtml.classList.toggle("porjects-active")
+})
+projectsBtn.addEventListener("click",()=>{
+  projectsHtml.classList.toggle("porjects-active")
+})
